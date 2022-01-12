4e5b83a343dccebe45bfc987de26834ea68b73f2
CrisGadea <cristianhernangadea@gmail.com>
2022-01-12 16:03:10 -0300

    First Commit

---
+++ funciones-flecha.js
@@ -0,0 +1,13 @@
+var add = (number1, number2) => number1 + number2;
+
+var sub = (number1, number2) => number1 - number2;
+
+var mult = (number1, number2) => number1 * number2;
+
+var div = (number1, number2) => number1 / number2;
+
+
+console.log("Add of 1 & 2: " + add(1,2));
+console.log("Substraction of 4 & 2: " + sub(4,2));
+console.log("multiplication of 12 & 2: " + mult(12,2));
+console.log("division of 8 & 2: " + div(8,2));
